73bb39e1c67063f9cf1f413394cf2cb20cf06751
islamnashaat03 <islamnashaat03@gmail.com>
2021-02-23 02:24:15 +0200

    create all pages

--- assets/js/main.js
+++ assets/js/main.js
@@ -20,4 +20,33 @@ $('.owl-header').owlCarousel({
             dots:true
         }
     }
-})
+});
+$('.owl-articles').owlCarousel({
+    loop:true,
+    margin:10,
+    rtl:true,
+    dots:true,
+    responsiveClass:true,
+    responsive:{
+        0:{
+            items:1,
+            nav:false
+        },
+        600:{
+            items:1,
+            nav:false
+        },
+        1000:{
+            items:3,
+            nav:true,
+            loop:true,
+            dots:false
+        }
+    }
+});
+$( function() {
+    $( "#datepicker" ).datepicker();
+  } );
+  $( function() {
+    $( "#datepicker-2" ).datepicker();
+  } );
